118d9344de345e066a86f35f788d0bbc6607c578
Jonathan Reis <jonathanreis7834@gmail.com>
2019-09-11 21:30:55 -0300

    Categorias lado administrador

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
 $app = new Slim();
 
@@ -196,6 +197,88 @@ $app->post("/admin/forgot/reset", function() {
 		"footer"=>false
 	]);
 	$pageAdmin->setTpl("forgot-reset-success");
+
+});
+
+$app->get("/admin/categories", function() {
+
+	User::verifyLogin();
+
+	$categories = Category::listAll();
+
+	$pageAdmin = new PageAdmin();
+	$pageAdmin->setTpl("categories", array(
+		"categories" => $categories
+	));
+
+});
+
+$app->get("/admin/categories/create", function() {
+
+	User::verifyLogin();
+
+	$pageAdmin = new PageAdmin();
+	$pageAdmin->setTpl("categories-create");
+
+});
+
+$app->post("/admin/categories/create", function() {
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);
+	$category->save();
+
+	header('Location: /admin/categories');
+	exit;
+
+});
+
+$app->get("/admin/categories/:idcategory/delete", function( $idcategory ) {
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+	$category->delete();
+
+	header('Location: /admin/categories');
+	exit;
+
+});
+
+$app->get("/admin/categories/:idcategory", function( $idcategory ) {
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$pageAdmin = new PageAdmin();
+	$pageAdmin->setTpl("categories-update", array(
+		"category"=>$category->getValues()
+	));
+
+});
+
+$app->post("/admin/categories/:idcategory", function( $idcategory ) {
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /admin/categories');
+	exit;
 
 });
 
